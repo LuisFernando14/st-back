4e1bba972c5b12a450a35e05789be95a36b04fa3
luis fernando martinez bueno <luisfernandomartinezbueno@MacBook-Pro-de-luis.local>
2019-12-02 00:24:48 -0700

    mas cambios

--- common/models/employee.js
+++ common/models/employee.js
@@ -1,6 +1,9 @@
 'use strict';
+// var app = require('../../server/server');
+// import Department from 'common/models/department.js'
 
 module.exports = function(Employee) {
+  var app = require('../../server/server');
   Employee.GetEmployees = function(cb) {
     let dataSource = Employee.dataSource;
     let cmd = `SELECT
@@ -8,6 +11,7 @@ module.exports = function(Employee) {
     FORMAT(e.FecNac, 'dd/MM/yyyy ') AS 'dob', d.Descripcion, e.Sueldo
     FROM Employee e INNER JOIN Department d ON e.Departamento = d.Puesto`;
     dataSource.connector.query(cmd, null, function(err, response) {
+      console.log('en el response dentro');
       if (err)
         console.log(err);
       console.log(response);
@@ -15,13 +19,14 @@ module.exports = function(Employee) {
     });
   };
 
+
   Employee.remoteMethod(
     'GetEmployees',
     {
       http: {path: '/pruebaTodos', verb: 'get'},
       description: 'Get list of employees',
       accepts: {},
-      returns: {arg: 'data', type: 'object'},
+      returns: {arg: 'data', type: []},
     }
   );
 };
